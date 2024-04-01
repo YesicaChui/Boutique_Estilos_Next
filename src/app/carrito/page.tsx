c94c67a10c445ee3da3e06c2a5333013a94ca526
YesicaChui <yesicachuic@gmail.com>
2024-03-31 20:25:06 -0500

    estilizando pagina carrito

--- src/app/carrito/page.tsx
+++ src/app/carrito/page.tsx
@@ -1,4 +1,5 @@
 import CardCarrito from '@/components/CardCarrito/CardCarrito';
+import data from '@/model/data';
 import Image from 'next/image';
 import React from 'react';
 
@@ -15,13 +16,57 @@ const CartPage = () => {
       <div className="flex py-5 gap-3">
         <div className="flex flex-col gap-5 w-[75%] ">
           <p className="text-4xl font-bold">Mi carrito S/. 556.00 PEN</p>
-          <CardCarrito/>
-          
+          <CardCarrito
+            category={data[0].category}
+            img={data[0].productImage}
+            name={data[0].productName}
+            price={data[0].productPrice}
+          />
+          <CardCarrito
+            category={data[4].category}
+            img={data[4].productImage}
+            name={data[4].productName}
+            price={data[4].productPrice}
+          />
+          <CardCarrito
+            category={data[10].category}
+            img={data[10].productImage}
+            name={data[10].productName}
+            price={data[10].productPrice}
+          />
+          <CardCarrito
+            category={data[15].category}
+            img={data[15].productImage}
+            name={data[15].productName}
+            price={data[15].productPrice}
+          />
         </div>
-        <div >
+        <div className="flex flex-col gap-8">
           <p className="text-4xl font-bold text-[#484848]">
             4 artículos en tu carrito de compras
           </p>
+          <div className="border"></div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold ">
+              Dela las instrucciones especiales a continuación:
+            </p>
+            <p className="text-2xl font-bold text-[#929292]">+</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-bold text-[#929292]">Subtotal</p>
+            <div className="text-lg font-bold text-[#929292]">S/. 556.00 PEN</div>
+          </div>
+          <div className="border"></div>
+          <div className="flex justify-between ">
+            <p className="text-lg font-bold">Total</p>
+            <p className="text-2xl font-bold">S/. 556.00</p>
+          </div>
+          <div className="flex justify-between">
+            <div></div>
+            <p className="text-white font-semibold text-xl bg-[#596E79] rounded-md p-3 text-center w-[50%] ">
+              Página de pago
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/CardCarrito/CardCarrito.tsx
+++ src/components/CardCarrito/CardCarrito.tsx
@@ -1,30 +1,28 @@
 import Image from 'next/image';
 import React from 'react';
-
-const CardCarrito = () => {
+interface CardCarritoProps {
+  img: string;
+  name: string;
+  category: string;
+  price: string;
+}
+const CardCarrito = (props: CardCarritoProps) => {
+  const { img, name, category, price } = props;
   return (
     <div className="border border-[#c6c6c6] py-2 px-4 w-[100%] rounded-md shadow relative">
       <p className="font-semibold text-3xl absolute right-5 text-[#797979]">x</p>
       <div className="flex gap-3 ">
-        <Image
-          src={
-            'https://topitop.vteximg.com.br/arquivos/ids/348293-302-385/2020812_2.jpg?v=638471031275200000'
-          }
-          alt=""
-          height={160}
-          width={160}
-          className="rounded-md"
-        />
+        <Image src={img} alt="" height={160} width={160} className="rounded-md" />
         <div className="flex justify-between flex-1 items-center gap-3">
-          <div>
-            <p className="text-lg text-[#484848] font-bold">Casaca Laguna Celeste</p>
-            <p className="text-base text-[#484848] font-semibold">Small</p>
+          <div className='w-[30%]'>
+            <p className="text-lg text-[#484848] font-bold">{name}</p>
+            <p className="text-base text-[#484848] font-semibold">{category}</p>
           </div>
-          <p className="text-xl font-medium">S/. 169.00</p>
+          <p className="text-xl font-medium">S/. {price}</p>
           <div className="flex gap-10 border border-[#c6c6c6] justify-between items-center p-3 rounded-md">
-            <p className=' text-4xl text-[#797979]'>-</p>
-            <p className='font-bold text-xl'>2</p>
-            <p className='font-bold text-3xl text-[#797979]'>+</p>
+            <p className=" text-4xl text-[#797979]">-</p>
+            <p className="font-bold text-xl">2</p>
+            <p className="font-bold text-3xl text-[#797979]">+</p>
           </div>
           <p className="text-xl font-medium">S/. 388.00</p>
         </div>
